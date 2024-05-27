53ecff4d25a6b77c25d17a4f3f84ef8d1fd4b8cf
Chidiro <hamzaduras48@gmail.com>
2024-05-27 11:49:40 +0300

    last

--- src/app/api/submit-survey/route.js
+++ src/app/api/submit-survey/route.js
@@ -6,7 +6,7 @@ export async function POST(request) {
     const { selectedValues } = await request.json();
     const selected = Object.entries(selectedValues);
     console.log(selected);
-    if (selected.length < 10) {
+    if (selected.length < 29) {
       throw new Error("Bütün soruları cevaplayın lütfen.");
     }
 
